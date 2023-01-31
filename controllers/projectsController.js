bc58dfff96112373b67fcab973f1a215115fe0c2
Alexis Peves <alexpeves@gmail.com>
2023-01-31 02:57:58 -0500

    routes moved to controller, user auth

---
+++ controllers/projectsController.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const router =  express.Router();
+
+// SCHEMA
+const Artist = require('../models/Schema.js');
+//SEED DATA
+const data = require('../models/data.js');
+
+//___________________
+// Routes
+//___________________
+//localhost:3000
+
+//---------------------FOR DEVELOPMENT PURPOSES ONLY---------------------
+//SEED ROUTE
+router.get('/seed',(req,res) => {
+    Artist.create(data,(err,data) => {
+        res.redirect('/')
+    })
+})
+//CLEAR DATABASE ROUTE
+router.get('/clear',(req,res) => {
+    Artist.deleteMany({},(err,deletedArtists) => {
+        res.redirect('/')
+    })
+})
+//---------------------FOR DEVELOPMENT PURPOSES ONLY---------------------
+
+//DELETE ROUTE
+router.delete('/:id',(req,res) => {
+    Artist.findByIdAndRemove(req.params.id, (err, deletedArtist) => {
+        res.redirect('/')
+    })
+})
+
+//EDIT ROUTE
+router.get('/:id/edit',(req,res) => {
+    Artist.findById(req.params.id, (err,foundArtist) => {
+        res.render('edit.ejs',{
+            artist:foundArtist
+        })      
+    })
+})
+
+//UPDATE ROUTE
+router.put('/:id',(req,res) => {
+    Artist.findByIdAndUpdate(req.params.id,req.body,{new:true},(err,updatedModel) => {
+        res.redirect('/')
+        // res.send(updatedModel)
+    })
+})
+
+//INDEX ROUTE
+router.get('/' , (req, res) => {
+    Artist.find({},(err,allArtists) => {
+        res.render('index.ejs', {
+            artist:allArtists
+        });      
+    })
+});
+
+//NEW ROUTE
+router.get('/new',(req,res) => {
+    res.render('new.ejs')
+})
+
+//CREATE ROUTE
+router.post('/',(req,res) => {
+    Artist.create(req.body, (err,createdArtist) => {
+        res.redirect('/')
+    })  
+})
+
+//SHOW ROUTE
+router.get('/show/:id',(req,res) => {
+    Artist.findById(req.params.id, (err,foundArtist) => {
+        res.render('show.ejs',{
+            artist:foundArtist
+        })
+    })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,126 +1,59 @@
-//___________________
 //Dependencies
-//___________________
 const express = require('express');
-const methodOverride  = require('method-override');
-const mongoose = require ('mongoose');
-const app = express ();
+const mongoose = require('mongoose');
+const session = require('express-session')
+const methodOverride = require('method-override');
+const app = express();
 const db = mongoose.connection;
-const Artist = require('./models/Schema.js')
-const data = require('./models/data.js')
 require('dotenv').config()
-//___________________
+
 //Port
-//___________________
-// Allow use of Heroku's port or your own local port, depending on the environment
 const PORT = process.env.PORT || 3003;
 
-//___________________
 //Database
-//___________________
-// How to connect to the database either via heroku or locally
 const MONGODB_URI = process.env.MONGODB_URI;
 
-// Connect to Mongo &
-// Fix Depreciation Warnings from Mongoose
-// May or may not need these depending on your Mongoose version
+//Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+
+//USER AUTH & SESSION
+const isAuthenticated = (req, res, next) => {
+    if (req.session.currentUser) {
+      return next()
+    } else {
+      res.redirect('/sessions/new')
+    }
+}
+app.use(
+    session({
+      secret: process.env.SECRET,
+      resave: false,
+      saveUninitialized: false
+    })
+)
+
+//Controllers
+const sessionsController = require('./controllers/sessionsController.js')
+app.use('/sessions', sessionsController)
+
+const userController = require('./controllers/usersController.js')
+app.use('/user', userController)
+
+const projectsController = require('./controllers/projectsController.js')
+app.use(projectsController)
+
+
+// Connect to Mongo
 mongoose.connect(MONGODB_URI);
 
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
-//___________________
-//Middleware
-//___________________
-
-//use public folder for static assets
-app.use(express.static('public'));
-
-// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
-app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
-
-//use method override
-app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
-
-
-//___________________
-// Routes
-//___________________
-//localhost:3000
-
-//SEED ROUTE
-app.get('/seed',(req,res) => {
-    Artist.create(data,(err,data) => {
-        res.redirect('/')
-    })
-})
-
-//CLEAR DATABASE ROUTE
-app.get('/clear',(req,res) => {
-    Artist.deleteMany({},(err,deletedArtists) => {
-        res.redirect('/')
-    })
-})
-
-//DELETE ROUTE
-app.delete('/:id',(req,res) => {
-    Artist.findByIdAndRemove(req.params.id, (err, deletedArtist) => {
-        res.redirect('/')
-    })
-})
-
-//EDIT ROUTE
-app.get('/:id/edit',(req,res) => {
-    Artist.findById(req.params.id, (err,foundArtist) => {
-        res.render('edit.ejs',{
-            artist:foundArtist
-        })      
-    })
-})
-
-//UPDATE ROUTE
-app.put('/:id',(req,res) => {
-    Artist.findByIdAndUpdate(req.params.id,req.body,{new:true},(err,updatedModel) => {
-        res.redirect('/')
-        // res.send(updatedModel)
-    })
-})
-
-//INDEX ROUTE
-app.get('/' , (req, res) => {
-    Artist.find({},(err,allArtists) => {
-        res.render('index.ejs', {
-          artist:allArtists
-        });      
-    })
-});
-
-//NEW ROUTE
-app.get('/new',(req,res) => {
-  res.render('new.ejs')
-})
-
-//CREATE ROUTE
-app.post('/',(req,res) => {
-    Artist.create(req.body, (err,createdArtist) => {
-        res.redirect('/')
-    })  
-})
-
-//SHOW ROUTE
-app.get('/show/:id',(req,res) => {
-    Artist.findById(req.params.id, (err,foundArtist) => {
-        res.render('show.ejs',{
-            artist:foundArtist
-        })
-    })
-})
-
-//___________________
 //Listener
-//___________________
 app.listen(PORT, () => console.log( 'Listening on port:', PORT));
 
